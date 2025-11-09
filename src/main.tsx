1d082ae4059ddba81bd6ea08bfb7a9bc8fdbb31a
vivek-dhanantar <103417952+vivek-dhanantar@users.noreply.github.com>
2025-11-09 11:59:11 +0530

    Refactor import statements and render method

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+//import { createRoot } from "react-dom/client";
+//import App from "./App.tsx";
+//import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+//createRoot(document.getElementById("root")!).render(<App />);"""
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
